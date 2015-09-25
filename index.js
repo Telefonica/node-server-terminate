8b2c58853c6bbc39d223db82e84ca6c2ed38bfd9
Jose Antonio Rodríguez <joseantonio.rodriguezfernandez@telefonica.com>
2015-09-25 11:35:50 +0200

    Fix copyright

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
  The MIT License (MIT)
 
- Copyright (c) 2015 Jose Antonio Rodríguez
+ Copyright (c) 2015 Telefónica Investigación y Desarrollo, S.A.U
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
